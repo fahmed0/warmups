af5b05bf16109880403693000f5583eecd38b873
fahmed0 <fahmed0199@gmail.com>
2024-09-08 00:03:02 +0100

    Initial commit

---
+++ my_script.py
@@ -0,0 +1,209 @@
+# coding: utf-8
+ipython
+ipython --version
+Ipython
+print "hello world"
+print ("hello world")
+def greet(name):
+    print("Hello, " + name)
+    print("Welcome to Python!")
+
+print("This line is outside the function")
+def greet(name):
+        print("Hello, " + name)
+            print("Welcome to Python!")
+print("This line is outside the function")
+if x > 0:
+    print("hello")
+    
+for i in [1,2,3,4,5]:
+    print(j)
+    print (i+j)
+print (i)
+print ("done looping")
+
+    
+for i in [1,2,3,4,5]:
+    print(i)
+    for j in [1,2,3,4,5]:
+        print(j)
+        print (i+j)
+   print(i)
+for i in [1,2,3,4,5]:
+    print(i)
+    for j in [1,2,3,4,5]:
+        print(j)
+        print(i+j)
+        print(i)
+        print("done looping")
+        
+for i in [1,2,3,4,5]:
+    print(i)
+    for j in [1,2,3,4,5]:
+        print(j)
+        print(i+j)
+           print(i)
+    print("done log)
+for i in [1, 2, 3, 4, 5]:
+    print(i)
+    for j in [1, 2, 3, 4, 5]:
+        print(j)
+        print(i + j)
+    print(i)
+print("done looping")
+for i in [1,2,3,4]:
+    print(i)
+    for j in [1,2,3,4]:
+        print (j)
+        print (i+j)
+    print (i)
+print ("done looping")
+jack_pebble = (2       + 3  +   17        +8)
+print(jack_pebble)
+jack = 2+ \
+3
+print(jack)
+import matplotlib as plt
+import matplotlib.pylplot as plt
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('conda', 'install matplotlib')
+import matplotlib.pyplot as plt
+plt.plot(2)
+print("weather)
+print("weather")
+def greet(jack):
+    return "Hello" + jack
+    
+print greet
+def greet(name):
+    return "Hello, " + name
+print (greet("alice"))
+print(greet("jack"))
+def add(a, b):
+    return a + b
+    
+result = add(3,7)
+print(result)
+def square(x):
+    return x * x
+    
+result = square(5)
+print(result)
+def greet(name = "World)
+def greet(name = "World")
+def greet( name = "World"):
+    return "Hello," + name
+    
+print(greet())
+def greet(name="World"):
+    return "Hello, " + name
+    
+print(greet())
+print(greet("Jack"))
+def greet(name):
+    """This function greets the person passed in as an argument."""
+    return "Hello, " + name
+ 
+print(greet(Jack))
+print(greet("Jack"))
+print(greet.__doc__)
+square = lambda x: x * x
+print(square(5))
+def calculate_area(r):
+    pi = 3.14159
+    area = pi *(r ** 2)
+    return area
+    
+r = 5
+area = calculate_area(r)
+print(f"The area of the circle with radius {r} is {area}")
+def double(x):
+    return x * 2
+   
+result = double(8)
+print(result)
+def my_print(message = " My default message. "):
+    print(message)
+    
+my_print
+my_print(hello)
+my_print()
+my_print("Hello")
+def full_name(first = "What's-his-name" , last= "Something"):
+    return first + "" + last
+    
+print(full_name())
+def full_name("Jack")
+print(full_name("Jack"))
+def full_name(first = "What's his name" , last= "Something"):
+    return first + " " + last
+    
+print(full_name("Jack"))
+print(full_name( "Jack" , "James"))
+print(full_name(last = "James"))
+get_ipython().run_line_magic('cd', '')
+dir
+get_ipython().run_line_magic('cd', '')
+get_ipython().run_line_magic('cd', '')
+C:\Users\44738\anaconda3\envs\dsfs
+get_ipython().run_line_magic('cd', '')
+get_ipython().run_line_magic('cd', '')
+get_ipython().run_line_magic('cd', 'C:\\Users\\44738\\anaconda3\\envs\\dsfs')
+git init
+git init 
+print("Hello\nWorld")
+print("Smiley: \u263A")
+print("Hello\tWorld")
+print("Hello World")
+print("Hello\tWorld")
+print("C:\\Users\\44738\\Documents")
+print('It'\s a nice day')
+print('It\'s a nice day')
+print('It\'s a nice day')
+print("He said "\Hello!\"")
+print("He said, \"Hello!\"")
+path = r"C:\Users\44738\Documents"
+print(path)
+raw_string = r"\t"
+print(raw_string)
+multi_line_string = """This is the first line
+This is the second line
+This is the third line"""
+print(multi_line_string)
+first_name = "Jack"
+last_name = "Edwards"
+full_name1 = first_name + "" + last_name
+print(full_name1)
+full_name1 = first_name + " " + last_name
+print(full_name1)
+test_raw = r"C:\"
+test_raw = r"C:\Users\44738\Documents\txt\"
+raw =  r"C:\Users\44738\Documents\"
+full_name2 = f"{first_name}  {last_name}"
+print(full_name2)
+full_name2 = f"{first_name} {last_name}"
+print(full_name2)
+print(0 / 0)
+print("Cannot divide by zero")
+integer_list = [1, 2, 3]
+hetrogenous_list = ["String", 0.1, True]
+list_of_lists = [integer_list, hetrogenous_list, []]
+list_length = len(integer_list)
+print(list_length)
+list_sum = sum(integer_list)
+print(list_sum)
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+zero = x[0]
+print(zero)
+seven = x[-3]
+print(seven)
+one = x[1]
+print(one)
+nine = x[-1]
+print(nine)
+eight = x[-2]
+print(eight)
+x[0] = -1
+print(x)
+m1 = x[0]
+print(m1)
